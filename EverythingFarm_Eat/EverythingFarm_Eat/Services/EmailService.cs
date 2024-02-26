4a503c98db01f5fb620d1fa91f78dabbbbdb80cc
Robert McCabe <rmccabe1023@gmail.com>
2024-02-26 15:41:46 -0500

    update files

---
+++ EverythingFarm_Eat/EverythingFarm_Eat/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EverythingFarm_Eat.Services
+{
+    public record EmailService
+    {
+        private readonly string smtpServer;
+        private readonly int smtpPort;
+        private readonly string smtpUsername;
+        private readonly string smtpPassword;
+
+        public EmailService(string smtpServer, int smtpPort, string smtpUsername, string smtpPassword) 
+        {
+            this.smtpServer = smtpServer;
+            this.smtpPort = smtpPort;
+            this.smtpUsername = smtpUsername;
+            this.smtpPassword = smtpPassword;
+        }
+
+        public async Task SendNotificationToClient(string clientEmail, string subject, string body) 
+        {
+            using (var client = new SmtpClient(smtpServer, smtpPort)) 
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                client.EnableSsl = true;
+
+                var message = new MailMessage(smtpUsername, clientEmail, subject, body);
+                await client.SendMailAsync(message);
+                
+            }
+        }
+    }
+}
