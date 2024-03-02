20e92a84c2a080b0c1d2873f248bbfe3464470d2
Robert McCabe <rmccabe1023@gmail.com>
2024-03-02 09:20:28 -0500

    update files

---
+++ EverythingFarm_Eat/EverythingFarm_Eat/Components/Models/ContactForm.cs
@@ -0,0 +1,25 @@
+﻿using Org.BouncyCastle.Crmf;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Sockets;
+
+namespace EverythingFarm_Eat.Components.Models
+{
+    public record ContactForm
+    {
+        
+        public required string ContactFirstName { get; set; }
+        
+        public required string ContactLastName { get; set;}
+        
+        public required string ContactEmail { get; set; }
+        public required string ContactPhone { get; set;}
+        public required string ContactStreetAddr { get; set; }
+        public string? ContactAddr2 { get; set; }
+        public required string ContactCity { get; set;}
+        public required string ContactState { get; set;}
+        public required string ContactZipCode { get; set;}
+        public required DateTime ReservationDate { get; set;}
+        public required TimeOnly ReservationTime { get; set;}
+        public required string  Message { get; set;}
+    }
+}
