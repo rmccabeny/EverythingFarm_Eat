4916c9b2fd23c6d56f5ab183b4bb6228053d6a82
Robert McCabe <rmccabe1023@gmail.com>
2024-03-10 19:45:37 -0400

    update files

--- EverythingFarm_Eat/EverythingFarm_Eat/Components/Models/CustomerForm.cs
+++ EverythingFarm_Eat/EverythingFarm_Eat/Components/Models/CustomerForm.cs
@@ -1,18 +1,24 @@
-﻿namespace EverythingFarm_Eat.Components.Models
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace EverythingFarm_Eat.Components.Models
 {
-    public record Customer
+    public record CustomerForm
     {
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Email { get; set; }
-        public required string PhoneNum { get; set; }
+        public required string Phone { get; set; }
         public required string StreetAddress { get; set; }
         public string? Address2 { get; set; }
         public required string City { get; set; }
         public required string State { get; set; }
         public required string ZipCode { get; set;}
         public required string ClientEmail { get; set;}
-
-       
+        public required string Message { get; set;}
+        public required bool Item { get; set; }
+        public required decimal Price { get; set; }
+        public required int Quantity { get; set; }
+        public required decimal Total { get; set; }
+        
     }
 }
