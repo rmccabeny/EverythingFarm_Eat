421bc0658b8725c17a7947157aab289b9e1fd2a1
Robert McCabe <rmccabe1023@gmail.com>
2024-03-22 15:14:44 -0400

    update files

--- EverythingFarm_Eat/EverythingFarm_Eat/Components/Models/Menu.cs
+++ EverythingFarm_Eat/EverythingFarm_Eat/Components/Models/Menu.cs
@@ -2,9 +2,18 @@
 {
     public record Menu
     {
+        public string? BreakfastChoice { get; set; }
         public string? Appetizer { get; set; }
-        public string? soupSalad { get; set; }
-
+        public string? SoupSalad { get; set; }
+        public string? BurgersDogs { get; set; }
+        public string? Subs { get; set; }
+        public string? EntreeChoice { get; set; }
+        public string? SideChoice { get; set; }
+        public string? AddOns { get; set; }
+        public string? KidsMenu { get; set; }
+        public string? FamStylOpt { get; set; }
+        public string? Drinks { get; set; }
+        public required decimal MenuPrice { get; set; }
 
 
     }
