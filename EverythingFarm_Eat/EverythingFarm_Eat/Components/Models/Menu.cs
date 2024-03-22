589dda427debaf89ab663f99ecace24a0fc293ef
Robert McCabe <rmccabe1023@gmail.com>
2024-03-22 10:53:34 -0400

    update file

---
+++ EverythingFarm_Eat/EverythingFarm_Eat/Components/Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿namespace EverythingFarm_Eat.Components.Models
+{
+    public record Menu
+    {
+        public string? Appetizer { get; set; }
+        public string? soupSalad { get; set; }
+
+
+
+    }
+}
