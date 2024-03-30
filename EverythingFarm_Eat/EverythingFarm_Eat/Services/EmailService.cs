4707c4963b63b25a23fa5e3508fd3595a6f5409e
Robert McCabe <rmccabe1023@gmail.com>
2024-03-30 13:07:48 -0400

    update files

--- EverythingFarm_Eat/EverythingFarm_Eat/Services/EmailService.cs
+++ EverythingFarm_Eat/EverythingFarm_Eat/Services/EmailService.cs
@@ -10,14 +10,9 @@ namespace EverythingFarm_Eat.Services
     }
 
     // create the email service utilizing the interface to create the email client
-    public class EmailService : IEmailService
+    public class EmailService(EmailClient emailClient) : IEmailService
     {
-        private readonly EmailClient emailClient;
-
-        public EmailService(EmailClient emailClient) 
-        {
-            this.emailClient = emailClient;
-        }
+        private readonly EmailClient emailClient = emailClient;
 
         // send the email through the service 
         public async Task SendEmailAsync(string from, string to, string subject, string message) 
